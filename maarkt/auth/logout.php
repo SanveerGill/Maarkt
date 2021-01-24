c0b1e881ec5e95f60cf0f3c73463e5f53d902c29
SanveerGill <77764509+SanveerGill@users.noreply.github.com>
2021-01-24 02:59:09 -0500

    Delete maarkt directory

--- maarkt/auth/logout.php
+++
@@ -1,6 +0,0 @@
-<!-- LOGOUT PAGE, handles when users log out of their account. Take them back to the main page index.php -->
-
-<?php
-session_start();
-unset($_SESSION['login']);
-header("Location: ../index.php");
